05dc91ea28b1c422cb029608ab185937fe1c757c
jyparashar <jyparashar@gmail.com>
2020-07-28 15:47:04 +0530

    Create ReportCard.sol

---
+++ Batch-1/Day-4/ReportCard.sol
@@ -0,0 +1,28 @@
+#LetsUpgrade-Blockchain-es
+Blockchain Essentials Code
+
+#Solidity File Code for ReportCard Project
+pragma solidity ^0.4.17 < 0.6.12;
+
+contract ReportCard{
+    
+    string public name;
+    uint public rollno;
+    string public batch;
+    uint public marks_sub1;
+    uint public marks_sub2;
+    uint public marks_sub3;
+    uint public marks_sub4;
+    string public status;
+    
+    function ReportCard(string newName, uint newRollNo, string newBatch, uint newMarks_sub1, uint newMarks_sub2, uint newMarks_sub3, uint newMarks_sub4, string newStatus) public{
+        name = newName;
+        rollno = newRollNo;
+        batch = newBatch;
+        marks_sub1 = newMarks_sub1;
+        marks_sub2 = newMarks_sub2;
+        marks_sub3 = newMarks_sub3;
+        marks_sub4 = newMarks_sub4;
+        status = newStatus;
+    }
+}
